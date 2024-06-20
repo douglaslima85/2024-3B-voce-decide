a7b1fbe5f56eb5f71b767f79babecbb13f3de376
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-06-20 08:29:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const pergunta =[
+    {
+        enunciado: "Enunciado 01",
+        Alternativas:[
+            "alternativas 01"
+            "alternativas 02"
+        ]
+    }       enunciado: "Enunciado 01",
+    Alternativas:[
+        "alternativas 01"
+        "alternativas 02"
+    ]       enunciado: "Enunciado 01",
+    Alternativas:[
+        "alternativas 01"
+        "alternativas 02"
+    ]       enunciado: "Enunciado 01",
+    Alternativas:[
+        "alternativas 01"
+        "alternativas 02"
+    ]       enunciado: "Enunciado 01",
+    Alternativas:[
+        "alternativas 01"
+        "alternativas 02"
+    ]      enunciado: "Enunciado 01",
+    Alternativas:[
+        "alternativas 01"
+        "alternativas 02"
+    ]       enunciado: "Enunciado 01",
+    Alternativas:[
+        "alternativas 01"
+        "alternativas 02"
+    ]
+]
