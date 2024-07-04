4cd2adc1a141cdcd4a0000cbe1abc9bb86cdaf8c
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-07-04 08:39:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,37 +3,60 @@ const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
-
 const pergunta =[
     {
-        enunciado: "Enunciado 01",
-        Alternativas:[
-            "alternativas 01"
-            "alternativas 02"
+        enunciado: "Pergunta 1",
+        Alternativas: [
+            "Alternativas 1",
+            "Alternativas 2"
         ]
-    }       enunciado: "Enunciado 01",
-    Alternativas:[
-        "alternativas 01"
-        "alternativas 02"
-    ]       enunciado: "Enunciado 01",
-    Alternativas:[
-        "alternativas 01"
-        "alternativas 02"
-    ]       enunciado: "Enunciado 01",
-    Alternativas:[
-        "alternativas 01"
-        "alternativas 02"
-    ]       enunciado: "Enunciado 01",
-    Alternativas:[
-        "alternativas 01"
-        "alternativas 02"
-    ]      enunciado: "Enunciado 01",
-    Alternativas:[
-        "alternativas 01"
-        "alternativas 02"
-    ]       enunciado: "Enunciado 01",
-    Alternativas:[
-        "alternativas 01"
-        "alternativas 02"
-    ]
+    }, 
+    {
+        enunciado: "Pergunta 1",
+        Alternativas: [
+            "Alternativas 1",
+            "Alternativas 2"
+        ]
+    },    
+    {
+        enunciado: "Pergunta 1",
+        Alternativas: [
+            "Alternativas 1",
+            "Alternativas 2"
+        ]
+    },  
+    {
+        enunciado: "Pergunta 1",
+        Alternativas: [
+            "Alternativas 1",
+            "Alternativas 2"
+        ]
+    },
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = pergunta[atual];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    mostraPergunta();
+}
+
+function mostraAlternativas(){
+    for(const alternativas of perguntaAtual.Alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativas;
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+        
+        
+    
+}
+
+function mostraPergunta(){
+    perguntaAtual = pergunta[atual];
+    caixaPergunta.textContent = perguntaAtual.enunciado
+}
+ 
+mostraPergunta();
