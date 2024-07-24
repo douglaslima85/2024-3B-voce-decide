506e1acab66f70a3bb588fb42c15199fd6018097
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-07-24 08:45:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,46 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
-const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaPergunta = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 const pergunta =[
     {
         enunciado: "Pergunta 1",
-        Alternativas: [
+        alternativas: [
             "Alternativas 1",
             "Alternativas 2"
         ]
     }, 
     {
-        enunciado: "Pergunta 1",
-        Alternativas: [
+        enunciado: "Pergunta 2",
+        alternativas: [
             "Alternativas 1",
             "Alternativas 2"
         ]
     },    
     {
-        enunciado: "Pergunta 1",
-        Alternativas: [
-            "Alternativas 1",
-            "Alternativas 2"
-        ]
-    },  
-    {
-        enunciado: "Pergunta 1",
-        Alternativas: [
+        enunciado: "Pergunta 3",
+        alternativas: [
             "Alternativas 1",
             "Alternativas 2"
         ]
     },
 ]
-
 let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
     perguntaAtual = pergunta[atual];
     caixaPergunta.textContent = perguntaAtual.enunciado;
-    mostraPergunta();
+    mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for(const alternativas of perguntaAtual.Alternativas){
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativas;
+        botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas)
-    }
-        
-        
-    
-}
-
-function mostraPergunta(){
-    perguntaAtual = pergunta[atual];
-    caixaPergunta.textContent = perguntaAtual.enunciado
+    }   
 }
  
 mostraPergunta();
