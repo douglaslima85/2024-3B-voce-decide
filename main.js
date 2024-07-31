a3985fcc1b29746ad730aad7b64a4cf09e147b13
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-07-31 08:43:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,40 +7,84 @@ const pergunta =[
     {
         enunciado: "Pergunta 1",
         alternativas: [
-            "Alternativas 1",
-            "Alternativas 2"
+            {
+                texto:"Alternativas 1",
+                afirmacao:  "resultado 1"
+            },
+            {
+                texto:"Alternativas 2",
+                afirmacao: "resultado 2"
+            }
+            
+          
         ]
     }, 
     {
         enunciado: "Pergunta 2",
         alternativas: [
-            "Alternativas 1",
-            "Alternativas 2"
+            {
+                texto:"Alternativas 3",
+                afirmacao: "resultado3"
+            },
+            {
+                texto:"Alternativas 4",
+                afirmacao: "resultado 4"
+            }
+            
+            
         ]
     },    
     {
         enunciado: "Pergunta 3",
         alternativas: [
-            "Alternativas 1",
-            "Alternativas 2"
+            {
+               texto:"Alternativas 5",
+               afirmacao: "resultado 5"
+            },
+            {
+               texto:"Alternativas 6",
+               afirmacao: "resultado 6"
+            }
+            
+            
         ]
     },
 ]
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = ""
 
-function mostraPergunta(){
+function mostraPergunta() {
+   if (atual >= pergunta.length){
+    mostraResultado();
+    return;
+   }
     perguntaAtual = pergunta[atual];
-    caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaPergunta.textContent = pergunta.enunciado;
+    caixaAlternativas.textContent="";
     mostraAlternativas();
+    
 }
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas)
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelacionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativas) 
     }   
+}
+function respostaSelacionada(opcaoSelacionada){
+    const afirmacao = opcaoSelacionada.afirmacao;
+    historiaFinal += afirmacao + " "
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado(){
+   caixaPergunta.textContent =" Inicio do texto..."
+   textoResultado.textContent = historiaFinal;
+   caixaAlternativas.textContent = "";
 }
  
 mostraPergunta();
