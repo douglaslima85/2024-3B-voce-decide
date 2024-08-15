dd8eabf64cbac1399da54169779187d417e3fb8f
Douglas dos Reis de Lima <106695841+douglaslima85@users.noreply.github.com>
2024-08-15 07:52:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 const pergunta =[
     {
-        enunciado: "voçê descobriu que recentemente o racismo e a discriminação vem aumentando",
+        enunciado: "pergunta 1",
         alternativas: [
             {
-                texto:"voçê descobriu que recentemente o racismo e a discriminação vem aumentando.",
-                afirmacao:  "resultado 1"
+                texto:"voçê descobriu que recentemente o racismo e a discriminação vem aumentando?",
+                afirmacao:  "Nos cenário atual, o preconceito e as práticas discriminação "
             },
             {
-                texto:"Alternativas 2",
+                texto:"Mais voçê não liga para esse assunto porque voçê acha que isso não e importante para voçê?",
                 afirmacao: "resultado 2"
             }
             
@@ -20,14 +20,14 @@ const pergunta =[
         ]
     }, 
     {
-        enunciado: "Mais voçê não liga para esse assunto porque voçê acha que isso não e importante para voçê?",
+        enunciado: "",
         alternativas: [
             {
-                texto:"Alternativas 3",
+                texto:"Aos 30 anos voçê viu pessoas fazendo racismo e discriminação pela cor da pele?",
                 afirmacao: "resultado3"
             },
             {
-                texto:"Alternativas 4",
+                texto:"Aos 33 anos voçê decidiu se juntar a um grupo contra o racismo e a discriminação?",
                 afirmacao: "resultado 4"
             }
             
@@ -36,6 +36,36 @@ const pergunta =[
     },    
     {
         enunciado: "Pergunta 3",
+        alternativas: [
+            {
+               texto:"Alternativas 5",
+               afirmacao: "resultado 5"
+            },
+            {
+               texto:"Alternativas 6",
+               afirmacao: "resultado 6"
+            }
+            
+            
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            {
+               texto:"Alternativas 5",
+               afirmacao: "resultado 5"
+            },
+            {
+               texto:"Alternativas 6",
+               afirmacao: "resultado 6"
+            }
+            
+            
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
         alternativas: [
             {
                texto:"Alternativas 5",
